00160b818f3b476fcf7f9a741f9619011fcb3564
lucas <lucasfbranco21@gmail.com>
2025-03-31 09:07:58 -0300

    declaraçoes e variaveis

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+// declaracoes e variantes
+
+var nome = "fiap;"
+console.log(nome)
+
+let idade = 19
+console.log(idade)
+
+const sobrenome="fabiho"
+console.log(sobrenome)
+
+// declarando uma variavel indefinida
+let nome1;
+console.log(nome1)
+// declarando uma variavel nula
+let nome2=nu11;
+console.log(nome2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
